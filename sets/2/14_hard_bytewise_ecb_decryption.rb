8ebb14696661fe7159df0e76f0bd5e58f95a7d55
Jason Colburne <j.colburne@gmail.com>
2021-12-13 07:04:43 -0400

    refactor analysis code into math gem

--- sets/2/14_hard_bytewise_ecb_decryption.rb
+++ sets/2/14_hard_bytewise_ecb_decryption.rb
@@ -13,85 +13,40 @@ dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
 YnkK
 EOT
 
-$key = SecureRandom.random_bytes(16)
-$secret_string = Base64.decode64(base64_string)
-$random_prefix = SecureRandom.random_bytes(SecureRandom.random_number(15) + 17)
-
-def encrypt(plain_text)
-  algorithm = :aes_128_ecb
-  cipher = Jason::Math::Cryptography::Cipher.new(algorithm, $key)
-  message = $random_prefix + plain_text + $secret_string
-  cipher.encrypt(message)
-end
-
-def detect_block_size(max_block_size = 64)
-  current_length = encrypt("A".b).length
-  
-  (2..(max_block_size + 1)).each do |i|
-    previous_length = current_length
-    current_length = encrypt("A".b * i).length
-    return current_length - previous_length if current_length != previous_length
+class Encryptor
+  def initialize(algorithm, key_length, secret_string)
+    key = SecureRandom.random_bytes(key_length)
+    @cipher = Jason::Math::Cryptography::Cipher.new(algorithm, key)
+    @random_prefix = SecureRandom.random_bytes(SecureRandom.random_number(15) + 17)
+    @secret_string = secret_string
   end
 
-  nil
-end
-
-def split_blocks(data, block_size)
-  block_count = data.length / block_size
-  (0..(block_count - 1)).map do |i|
-    data[(i * block_size)..((i + 1) * block_size - 1)]
+  def encrypt(plain_text)
+    message = @random_prefix + plain_text + @secret_string
+    @cipher.encrypt(message)
   end
 end
 
-def determine_combined_length(block_size)
-  current_length = encrypt("").size
-  (1..(block_size - 1)).each do |i|
-    previous_length = current_length
-    current_length = encrypt("A".b * i).size
-    return previous_length - i if current_length != previous_length
-  end
-end
-
-def determine_prefix_length(block_size)
-  current_blocks = split_blocks(encrypt(""), block_size)
-  first_difference = nil
-  (1..block_size).each do |i|
-    previous_blocks = current_blocks
-    current_blocks = split_blocks(encrypt("A".b * i), block_size)
-    changed = false
-    previous_blocks.each_with_index do |block, index|
-      if block != current_blocks[index]
-        if first_difference.nil?
-          first_difference = index
-        elsif first_difference != index
-          changed = true
-        end
-
-        break
-      end
-    end
-
-    return first_difference * block_size + (1 - i) % block_size if changed
-  end
-end
-
-block_size = detect_block_size(64)
+encryptor = Encryptor.new(:aes_192_ecb, 24, base64_string.base64_to_byte_string)
+block_size = Jason::Math::Cryptography::Cipher.block_size(encryptor)
 puts "block size: #{block_size}"
-puts "ecb? #{Jason::Math::Cryptography::Cipher.detect_ecb?(encrypt("A".b * 48))}"
+ecb = Jason::Math::Cryptography::Cipher.detect_ecb?(encryptor.encrypt("A".b * 48))
+puts "ecb? #{ecb}"
+exit(1) unless ecb
 
-combined_length = determine_combined_length(block_size)
-prefix_length = determine_prefix_length(block_size)
-secret_length = combined_length - prefix_length
+extra_length = Jason::Math::Cryptography::Cipher.count_clear_text_extra_bytes(encryptor, block_size)
+prefix_length = Jason::Math::Cryptography::Cipher.count_clear_text_prefix_bytes(encryptor, block_size)
+secret_length = extra_length - prefix_length
 
 secret_string = ""
 secret_length.times do |i|
   base_clear_text = "A".b * ((-secret_string.length - prefix_length - 1) % block_size)
   length = prefix_length + base_clear_text.length + secret_string.length
-  target = encrypt(base_clear_text)[0..length]
+  target = encryptor.encrypt(base_clear_text)[0..length]
 
   (0..255).each do |n|
     char = n.chr
-    potential_match = encrypt(base_clear_text + secret_string + char)[0..length]
+    potential_match = encryptor.encrypt(base_clear_text + secret_string + char)[0..length]
 
     if potential_match == target
       print char
