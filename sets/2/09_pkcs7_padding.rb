3d9273135032299c93de4c4f6bd8754eefbd3d94
Jason Colburne <j.colburne@gmail.com>
2021-12-12 20:02:21 -0400

    refactor to bump math gem

--- sets/2/09_pkcs7_padding.rb
+++ sets/2/09_pkcs7_padding.rb
@@ -4,4 +4,4 @@ require 'rubygems'
 require 'bundler/setup'
 require 'jason/math'
 
-pp Jason::Math::Cryptography.pad_pkcs7(ARGV[0], ARGV[1].to_i)
+pp Jason::Math::Cryptography::PKCS7.pad(ARGV[0], ARGV[1].to_i)
