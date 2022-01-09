889e5cfc34d0b98c3fdef4c862deb58f5da973df
Jason Colburne <j.colburne@gmail.com>
2022-01-09 03:18:13 -0400

    set 5 #36 #37

---
+++ sets/5/36_secure_remote_password.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'jason/math'
+
+$VERBOSE = nil
+
+Cryptography = Jason::Math::Cryptography
+
+# agreed
+N = Cryptography::KeyAgreement::DiffieHellman::PARAMETERS[:'3072'][:p].to_i(16)
+g = 2
+k = 3
+I = 'address@domain.org'
+P = 'password'
+
+# server precomputes (discarding x)
+salt = SecureRandom.random_bytes(16)
+sha = Cryptography::Digest::SecureHashAlgorithm.new(:'3_256')
+x = sha.digest(salt + P).byte_string_to_integer
+v = g.modular_exponentiation(x, N)
+
+# protocol
+
+# client
+a = SecureRandom.random_bytes(32).byte_string_to_integer % N
+A = g.modular_exponentiation(a, N)
+
+# client ---I,A--> server
+
+# server
+b = SecureRandom.random_bytes(32).byte_string_to_integer % N
+B = (k * v + g.modular_exponentiation(b, N)) % N
+
+# server ---salt,B--> client
+
+# both
+u = sha.digest(A.to_byte_string + B.to_byte_string).byte_string_to_integer
+
+# client
+# x = sha.digest(salt + P).byte_string_to_integer
+S = (B - k * g.modular_exponentiation(x, N)).modular_exponentiation((a + u * x), N)
+Kc = sha.digest(S.to_byte_string)
+
+# server
+S = (A * v.modular_exponentiation(u, N)).modular_exponentiation(b, N)
+Ks = sha.digest(S.to_byte_string)
+
+hmac = Cryptography::MessageAuthentication::HashedMessageAuthenticationCode.new(:sha_3_256, salt)
+tag = hmac.tag(Kc)
+valid_tag = hmac.tag(Ks)
+
+raise 'server failed to validate request' unless Cryptography.secure_compare(tag, valid_tag)
+puts 'password validated'
---
+++ sets/5/37_break_secure_remote_password_with_zero_key.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'jason/math'
+
+$VERBOSE = nil
+
+Cryptography = Jason::Math::Cryptography
+
+# agreed
+N = Cryptography::KeyAgreement::DiffieHellman::PARAMETERS[:'3072'][:p].to_i(16)
+g = 2
+k = 3
+I = 'address@domain.org'
+P = 'password'
+
+# server precomputes (discarding x)
+salt = SecureRandom.random_bytes(16)
+sha = Cryptography::Digest::SecureHashAlgorithm.new(:'3_256')
+x = sha.digest(salt + P).byte_string_to_integer
+v = g.modular_exponentiation(x, N)
+
+# client
+A = 0
+
+# server
+b = SecureRandom.random_bytes(32).byte_string_to_integer % N
+B = (k * v + g.modular_exponentiation(b, N)) % N
+
+hmac = Cryptography::MessageAuthentication::HashedMessageAuthenticationCode.new(:sha_3_256, salt)
+
+# client
+Kc = sha.digest(0.to_byte_string)
+
+# server
+u = sha.digest(A.to_byte_string + B.to_byte_string).byte_string_to_integer
+Ss = (A * v.modular_exponentiation(u, N)).modular_exponentiation(b, N)
+Ks = sha.digest(Ss.to_byte_string)
+
+tag = hmac.tag(Kc)
+valid_tag = hmac.tag(Ks)
+
+raise 'could not validate password' unless Cryptography.secure_compare(tag, valid_tag)
+puts 'password validated (null A)' 
