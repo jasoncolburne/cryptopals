5ba40ae78ca45efec2dddbdf7363871d89e27d66
Jason Colburne <j.colburne@gmail.com>
2022-01-10 01:29:42 -0400

    cleanup

--- sets/5/40_rsa_broadcast_attack.rb
+++ sets/5/40_rsa_broadcast_attack.rb
@@ -7,21 +7,11 @@ require 'jason/math'
 message = ARGV[1].byte_string_to_integer
 rsa = Jason::Math::Cryptography::AsymmetricKey::RivestShamirAdleman.new(ARGV[0].to_sym, nil, nil, 3)
 
-puts 'generating keypair a...'
-na, _, _ = rsa.generate_keypair!
-cipher_text_a = rsa.encrypt(message)
-puts 'generating keypair b...'
-nb, _, _ = rsa.generate_keypair!
-cipher_text_b = rsa.encrypt(message)
-puts 'generating keypair c...'
-nc, _, _ = rsa.generate_keypair!
-cipher_text_c = rsa.encrypt(message)
-
-mapping = {
-  na => cipher_text_a,
-  nb => cipher_text_b,
-  nc => cipher_text_c
-}
+mapping = 3.times.map do |i|
+  puts "generating keypair #{i}..."
+  n, _, _ = rsa.generate_keypair!
+  [n, rsa.encrypt(message)]
+end.to_h
 
 remainder = mapping.chinese_remainder_theorem(enforce_co_primality: false)
 puts "recovered: #{remainder.root(3).to_byte_string}"
